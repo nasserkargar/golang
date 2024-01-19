973e9b18b13c6f9c78465915a481d211d065964d
nasserkargar <63549097+nasserkargar@users.noreply.github.com>
2024-01-19 14:52:10 +0330

    Create main.go

---
+++ https-get/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("https://www.ietf.org/rfc/rfc2795.txt")
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		defer resp.Body.Close()
+		io.Copy(os.Stdout, resp.Body)
+	}
+}
+	
